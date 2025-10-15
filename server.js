87554522be2963f550c3216c64c3995aee20d136
AsforDounia <asfordounia@gmail.com>
2025-10-15 15:22:39 +0100

    add Mocha/Chai test configuration and an example for test

--- server.js
+++ server.js
@@ -60,6 +60,11 @@ app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+export default app;
