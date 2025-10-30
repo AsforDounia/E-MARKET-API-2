6acdc3b2e270ec6994bf46618527b5a0911aa25c
AsforDounia <asfordounia@gmail.com>
2025-10-30 16:00:11 +0100

    Enhance user registration and login responses; add password confirmation check

--- migrations/seed.js
+++ migrations/seed.js
@@ -1,3 +1,4 @@
+
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import { faker } from '@faker-js/faker';
@@ -88,10 +89,11 @@ async function seedDatabase() {
 }
 
 export default seedDatabase;
+seedDatabase();
 
 // Si on exécute ce fichier directement avec `node seed.js`
-if (import.meta.url === `file://${process.argv[1]}`) {
-  seedDatabase()
-    .then(() => process.exit(0))
-    .catch(() => process.exit(1));
-}
+// if (import.meta.url === `file://${process.argv[1]}`) {
+//   seedDatabase()
+//     .then(() => process.exit(0))
+//     .catch(() => process.exit(1));
+// }
